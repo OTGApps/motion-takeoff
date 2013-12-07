ad542e5a1de9068e1e65c74dcb3c0415b2757055
Mark Rickert <mjar81@gmail.com>
2013-12-07 12:59:37 -0500

    Simplify the reminders specs.

--- spec/reminders.rb
+++ spec/reminders.rb
@@ -2,8 +2,10 @@ describe "Reminders Spec" do
 
   before do
     MotionTakeoff::Reminders.reset
-    @body = "Specs are awesome"
-    @action = "Do It!"
+    @default = {
+      body: "Specs are awesome",
+      fire_date: 10
+    }
   end
 
   after do
@@ -11,112 +13,73 @@ describe "Reminders Spec" do
   end
 
   it "raises error when no body" do
-    should.raise { MotionTakeoff::Reminders.schedule(fire_date: 10) }
+    should.raise { MotionTakeoff::Reminders.schedule(@default.except(:fire_date)) }
   end
 
   it "raises error when no fire_date" do
-    should.raise { MotionTakeoff::Reminders.schedule(body: @body) }
+    should.raise { MotionTakeoff::Reminders.schedule(@default.except(:body)) }
   end
 
   it "should create a notification" do
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: 10
-    )
+    MotionTakeoff::Reminders.schedule(@default)
     UIApplication.sharedApplication.scheduledLocalNotifications.count.should == 1
   end
 
   it "should create multiple notifications" do
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: 10
-    )
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: 20
-    )
+    MotionTakeoff::Reminders.schedule(@default)
+    MotionTakeoff::Reminders.schedule(@default.merge({fire_date: 20}))
     UIApplication.sharedApplication.scheduledLocalNotifications.count.should == 2
   end
 
   it "should clear all notifications" do
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: 10
-    )
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: 20
-    )
+    MotionTakeoff::Reminders.schedule(@default)
+    MotionTakeoff::Reminders.schedule(@default.merge({fire_date: 20}))
     UIApplication.sharedApplication.scheduledLocalNotifications.count.should == 2
     MotionTakeoff::Reminders.reset
     UIApplication.sharedApplication.scheduledLocalNotifications.count.should == 0
   end
 
   it "should accept an integer for Time" do
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: 20
-    )
+    MotionTakeoff::Reminders.schedule(@default)
     notification = UIApplication.sharedApplication.scheduledLocalNotifications.first
     notification.fireDate.is_a?(NSDate).should.be.true
   end
 
   it "should accept a Time object" do
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: Time.now + 20
-    )
+    MotionTakeoff::Reminders.schedule(@default.merge({fire_date: Time.now + 20}))
     notification = UIApplication.sharedApplication.scheduledLocalNotifications.first
     notification.fireDate.is_a?(NSDate).should.be.true
   end
 
   it "should properly set the body of the message" do
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: Time.now + 20
-    )
+    MotionTakeoff::Reminders.schedule(@default)
     notification = UIApplication.sharedApplication.scheduledLocalNotifications.first
-    notification.alertBody.should == @body
+    notification.alertBody.should == @default[:body]
   end
 
   it "should set the action" do
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: Time.now + 20,
-      action: @action
-    )
+    action = "Do It!"
+    MotionTakeoff::Reminders.schedule(@default.merge({action: action}))
     notification = UIApplication.sharedApplication.scheduledLocalNotifications.first
-    notification.alertAction.should == @action
+    notification.alertAction.should == action
   end
 
   it "should allow repeating" do
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: Time.now + 20,
-      repeat: NSMinuteCalendarUnit
-    )
+    MotionTakeoff::Reminders.schedule(@default.merge({repeat: NSMinuteCalendarUnit}))
     notification = UIApplication.sharedApplication.scheduledLocalNotifications.first
     notification.repeatInterval.should == NSMinuteCalendarUnit
   end
 
   it "should set a sound file name" do
     sound = "alert"
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: Time.now + 10,
-      sound: sound
-    )
+    MotionTakeoff::Reminders.schedule(@default.merge({sound: sound}))
     notification = UIApplication.sharedApplication.scheduledLocalNotifications.first
     notification.soundName.should == sound
   end
 
   it "should set the user_info object" do
     user_info = {name: "Mark Rickert", project: "motion-takeoff"}
-    MotionTakeoff::Reminders.schedule(
-      body: @body,
-      fire_date: Time.now + 10,
-      user_info: user_info
-    )
+    MotionTakeoff::Reminders.schedule(@default.merge({user_info: user_info}))
     notification = UIApplication.sharedApplication.scheduledLocalNotifications.first
     notification.userInfo.should == user_info.stringify_keys
   end
